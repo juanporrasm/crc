ccb7e559214bda3746b3bdc53e75b97b1ed3527a
juanporrasm <japorras@gmail.com>
2025-11-12 13:44:34 +0100

    Reorder podman installation command parameters

--- scripts/comun4.sh
+++ scripts/comun4.sh
@@ -31,7 +31,7 @@ sudo yum -y install sshpass psmisc nmap mlocate dos2unix tcpdump python3-pip
 sudo yum -y install nc htop strace man man-pages-overrides sysstat unzip dmidecode  
 sudo yum -y install bind-utils bash-completion  net-tools inotify-tools git
 sudo figlet -c "Contenedores"
-sudo dnf install -y  podman-compose podman-plugins conmon containers-common container-tools golang netavark kubernetes-kubectl helm httpd-tools
+sudo dnf install -y container-tools containers-common-extra containers-common conmon podman-compose podman-plugins golang netavark kubernetes-kubectl helm httpd-tools
 #sudo dnf -y install golang netavark kubernetes-kubectl git skopeo helm httpd-tools
 # man-pages-es http python-pip lynx
 ### Actualiza base de datos de locate ###
