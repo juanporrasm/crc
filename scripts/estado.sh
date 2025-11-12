70814a60aba932ca61133277ec537832b0bb8820
juanporrasm <japorras@gmail.com>
2025-11-12 14:35:38 +0100

    Update estado.sh

--- scripts/estado.sh
+++ scripts/estado.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+TERM=xterm-256color
 estado=$(crc status|head -1|awk '{print $3}')
 if [[ $estado = "Stopped" ]]; then 
-   echo "parado"
-   echo $estado
-   echo "Lo arranco"
-   crc start |tee -a credenciales
+  tput setaf 1; echo "parado"; tput sgr0
+  tput setaf 214; echo $estado; tput sgr0
+  tput setaf 48; echo "Lo arranco"; tput sgr0
+  crc start |tee -a credenciales
 else 
-  echo "Arrancado"
-  echo $estado
+  tput setaf 48; echo "Arrancado"; tput sgr0
+  tput setaf 214; echo $estado ; tput sgr0 
 fi
-
+tput setaf 226
+cat /home/vagrant/credenciales
+tput sgr0
+tput setaf 153
+crc console --credentials
+tput sgr0
 
